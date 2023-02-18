6412259be18f5f1b138e57147a715902abd9acae
daesanTheRiceologist <dkim265@my.bcit.ca>
2023-02-18 13:14:02 -0800

    input text added

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,23 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return (<>
+    <input type="text" />
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='stairs'>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='stairs2'>
+      
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='stairs3'>
+      
+      </div>
+      <div className='stairs4'>
+      
+      </div>
     </div>
+    </>
   )
 }
 
