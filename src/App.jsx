cbff300fa13f5509eb5acc224b4f5aea5fc1a596
Sukhpal Singh Gosal <sukhpal_gosal@hotmail.com>
2023-02-18 18:42:12 -0800

    Add multiple languages for translation

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
   const [xMove, setXMove] = useState(100)
   const [yMove, setYMove] = useState(0)
   const [blockOne, setBlockOne] = useState(true)
-
+  let language = " ";
   const handleClick = () => {
     setXMove(xMove + 145)
     setYMove(yMove - 70)
+    
   }
 
-  let translateFrom = "en-GB";
-  let translateTo = "fr-FR";
+  let translateFrom = "en";
 
   let userInput, outputDisplayTextBox;
 
@@ -24,6 +24,13 @@ function App() {
       event.preventDefault();
       userInput = document.getElementById("user-input").value;
       console.dir(userInput);
+
+      let list = document.querySelector("#input-language");
+      language = list.value;
+      console.log(language);
+      let translateTo = language;
+
+
       let apiUrl = `https://api.mymemory.translated.net/get?q=${userInput}&langpair=${translateFrom}|${translateTo}`;
       fetch(apiUrl)
           .then(res => {
@@ -37,20 +44,27 @@ function App() {
               outputDisplayTextBox = document.getElementById("output-display");
               outputDisplayTextBox.value = data.responseData.translatedText;
           })
-  })
-
+  });
+  
   return (<>
     <motion.img animate={{ x: xMove, y: yMove, transition: { duration: 1 } }}  id='mario' src="mario.png" alt="" srcset="" />
+    <div className='mainContainer'>
     <form id="translate-form">
-      <div className='mainContainer'>
+     
       <textarea id="user-input" placeholder='Word'></textarea>
-      <textarea id="output-display" placeholder='User Guess' ></textarea>
+      <textarea disable id="output-display" placeholder='User Guess' ></textarea>
       <button onClick={()=> handleClick()}id="submit">Submit</button>
-      </div>
-    
+      
     </form>
- 
-    </>
+    <select id="input-language">
+      <option value="fr">French</option>
+      <option value="es">Spanish</option>
+      <option value="ja">Japanese</option>
+      <option value="hi">Hindi</option>
+      <option value="ru">Russian</option>
+    </select>
+    </div>
+    </> 
   )
 }
 
