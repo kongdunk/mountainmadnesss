0e64983a140b032a3bbb6ab1148329d7af235839
daesanTheRiceologist <dkim265@my.bcit.ca>
2023-02-18 16:54:17 -0800

    mario movement added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import './App.css'
 import { motion } from 'framer-motion'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [xMove, setXMove] = useState(100)
+  const [yMove, setYMove] = useState(0)
   const [blockOne, setBlockOne] = useState(true)
 
+  const handleClick = () => {
+    setXMove(xMove + 145)
+    setYMove(yMove - 70)
+  }
+
   return (<>
-      <img id='mario' src="mario.png" alt="" srcset="" />
+    <button onClick={() => handleClick()}> move mario </button>
+    <motion.img animate={{ x: xMove, y: yMove, transition: { duration: 1 } }}  id='mario' src="mario.png" alt="" srcset="" />
     <div className='mainContainer'>
     <input type="text" />
     </div>
